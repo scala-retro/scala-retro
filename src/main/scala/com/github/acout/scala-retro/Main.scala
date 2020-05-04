256ec6f25c77ebfaa941b50bbccd4e9786c57479
acout <anthony.coutant@gmail.com>
2020-05-04 12:37:30 +0200

    Output file is now also a command line arg

--- src/main/scala/com/github/acout/scala-retro/Main.scala
+++ src/main/scala/com/github/acout/scala-retro/Main.scala
@@ -6,7 +6,7 @@ object Main extends App{
 
     val files = Utils.getAllScalaFiles(new File(args.head).toPath)
     val tokens = new ScalaTokenizer().tokenize(files)
-    val writer = new MermaidClassDiagramWriter(new FileWriter(new File("output.md")))
+    val writer = new MermaidClassDiagramWriter(new FileWriter(new File(args(1))))
     writer.write(tokens)
     writer.close
 
