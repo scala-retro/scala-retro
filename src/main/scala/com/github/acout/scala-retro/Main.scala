6767cd15e504966e8ebb653264ca5d939dfa30dc
acout <anthony.coutant@gmail.com>
2020-05-04 14:23:12 +0200

    Added filtering on class names

--- src/main/scala/com/github/acout/scala-retro/Main.scala
+++ src/main/scala/com/github/acout/scala-retro/Main.scala
@@ -4,10 +4,18 @@ import java.io.{File, FileWriter}
 
 object Main extends App{
 
+    val regex = if(args.length > 2) args(2) else ".*"
+
     val files = Utils.getAllScalaFiles(new File(args.head).toPath)
     val tokens = new ScalaTokenizer().tokenize(files)
     val writer = new MermaidClassDiagramWriter(new FileWriter(new File(args(1))))
-    writer.write(tokens)
+    writer.write(tokens.filter(t => {
+        t match {
+            case ClassToken(name, _, _) => name.matches(regex)
+            case InheritanceToken(from, to) => from.matches(regex) && to.matches(regex)
+            case AssociationToken(from, to) => from.matches(regex) && to.matches(regex)
+        }
+    }))
     writer.close
 
 }
